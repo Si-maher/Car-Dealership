46096bb1345eaf11c4e236f0e055a5b1859791c4
Simon Maher <simon.maher@integrify.io>
2019-10-03 13:54:55 +0300

    Created create car function, car constructor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 window.addEventListener('load', () => 
     
     document.querySelector('.preloader').classList.add('hidePreloader'))
- 
+//  car data 
+
+const CreateCars = (() => {
+    const cars = [];
+
+    class Car {
+        constructor(make,country,img,special,model,price,type,trans,gas) {
+            this.make = make ;
+            this.country = country ;
+            this.img = img ;
+            this.special = special ;
+            this.model = model ;
+            this.price = price ;
+            this.type = type ;
+            this.trans = trans ;
+            this.gas = gas ;
+        }
+    }
+
+    
+})();
