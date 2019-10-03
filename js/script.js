2d33080d983da90798426028cd5e5e18b358a0ce
Simon Maher <simon.maher@integrify.io>
2019-10-03 15:55:56 +0300

    created function to display featured cars dynamically, added eventListeners

--- js/script.js
+++ js/script.js
@@ -19,38 +19,68 @@ const CreateCars = (() => {
             this.gas = gas ;
         }
     }
-const makeCar =(make, country, img='img/car-default.jpeg', special=true,model='new model', price=10000, type='sedan',trans='automatic', gas='50')=> {
+function makeCar(make, country, img='img/car-default.jpeg', special=true,model='new model', price=10000, type='sedan',trans='automatic', gas='50') {
     const car = new Car(make,country,img,special,model,price,type,trans,gas);
     cars.push(car)
 }
 // produce cars 
 
-const produceCars = () => {
+function produceCars() {
     makeCar('chevy','american');
     makeCar('mercedes', 'german', 'img/car-german-1.jpeg', true);
-    makeCar('mercedes', 'german', 'img/car-german-2.jpeg');
-    makeCar('mercedes', 'german', 'img/car-german-3.jpeg',false,'some model');
-    makeCar('mercedes', 'german', 'img/car-german-4.jpeg', undefined,'other model');
-    makeCar('mercedes', 'german', 'img/car-german-5.jpeg', true);
+    makeCar('bmw', 'german', 'img/car-german-2.jpeg');
+    makeCar('bmw', 'german', 'img/car-german-3.jpeg',false,'some model');
+    makeCar('bmw', 'german', 'img/car-german-4.jpeg', undefined,'other model');
+    makeCar('mercedes', 'german', 'img/car-german-5.jpeg', false);
     makeCar('chevy', 'amercian','img/car-american-1.jpeg');
     makeCar('chevy', 'amercian','img/car-american-2.jpeg',false);
-    makeCar('chevy', 'amercian','img/car-american-3.jpeg');
-    makeCar('chevy', 'amercian','img/car-american-4.jpeg');
-    makeCar('chevy', 'amercian','img/car-american-5.jpeg');
+    makeCar('chevy', 'amercian','img/car-american-3.jpeg',false);
+    makeCar('chevy', 'amercian','img/car-american-4.jpeg',false);
+    makeCar('chevy', 'amercian','img/car-american-5.jpeg',false);
 }
-const SpecialCars = cars.filter(car => car.special === true)
-return (
+produceCars()
+console.log(cars)
+
+const specialCars = cars.filter(car => car.special === true)
+console.log(specialCars)
+return {
+
     cars,
-    SpecialCars
-)
+    specialCars
+}
+
 })();
 
 
 const FeaturedCars = ((CreateCars)=>{
-const SpecialCars = CreateCars.SpecialCars
+const specialCars = CreateCars.specialCars
+console.log(specialCars)
 const info = document.querySelector('.featured-info');
 document.addEventListener('DOMContentLoaded', ()=> {
     info.innerHTML='';
+
+    let data ='';
+    specialCars.forEach(item => {data += `
+<div class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap">
+    <span data-img='${item.img}' class="featured-icon mr-2">
+        <i class="fas fa-car"></i>
+    </span>
+    <h5 class="font-weight-bold mx-1">${item.make}</h5>
+    <h5 class="mx-1">${item.model}</h5>
+
+</div>
+`
+    })
+    info.innerHTML = data
+})
+// Changing the image dynamically
+
+info.addEventListener('click', (event) => {
+    if(event.target.parentElement.classList.contains('featured-icon')){
+        const img = event.target.parentElement.dataset.img;
+        document.querySelector('.featured-photo').src = img
+    }
+    
 })
 })(CreateCars)
 
